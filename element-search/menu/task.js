952f44e50fdcee9ebf41569708900fe2cf0da449
Karim <k.kron2015@yandex.ru>
2020-03-23 23:56:23 +0300

    after reworking 1.2 (first)

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,15 +5,22 @@ const arr_items = Array.from(items);
 
 arr_items.forEach(item => {
   let elementDropDown = item.parentElement.getElementsByTagName('ul');
-  if (elementDropDown.length !== 0){
+  if (elementDropDown.length !== 0) {
     let className = elementDropDown[0].className;
+    elementDropDown[0].counter = 0;
+
     item.onclick = () => {
       let arr_menuSub = Array.from(document.getElementsByClassName('menu_sub'));
       arr_menuSub.forEach(i => {
         i.className = className;
-        console.log(i.className);
       });
-      item.parentElement.getElementsByTagName('ul')[0].className += ' menu_active';
+      if (item.parentElement.getElementsByTagName('ul')[0].counter == 1) {
+        item.parentElement.getElementsByTagName('ul')[0].counter = 0;
+      } else {
+        item.parentElement.getElementsByTagName('ul')[0].counter += 1;
+        item.parentElement.getElementsByTagName('ul')[0].className += ' menu_active';
+      }
+
       return false;
     }
 
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -4,51 +4,53 @@ const arr_sliderItems = Array.from(document.getElementsByClassName('slider__item
 const arr_sliderDot = Array.from(document.getElementsByClassName('slider__dot'));
 const sliderPrev = document.querySelector('div.slider__arrow_prev');
 const sliderNext = document.querySelector('div.slider__arrow_next');
-let counter = 0;
 
-arr_sliderDot[counter].className += ' slider__dot_active';
+
+function assignClassName(arr, className) {
+  arr.forEach(item => {
+    item.className = className
+  });
+}
+
+function getImageNumber(arr, fullActiveClassName) {
+  let num;
+  arr.forEach((item, i) => {
+    if (item.className == fullActiveClassName) num = i;
+  });
+  return num;
+}
+
+function setActiveElement(arr, i, activeClassName) {
+  arr[i].className += activeClassName;
+}
+
+function drawing(arr, i, className, activeClassName) {
+  assignClassName(arr, className);
+  setActiveElement(arr, i, activeClassName);
+}
+
+let num = getImageNumber(arr_sliderItems, 'slider__item slider__item_active');
+setActiveElement(arr_sliderDot, num, ' slider__dot_active');
+
+
 
 sliderPrev.onclick = function () {
-  arr_sliderItems.forEach((item, i) => {
-    item.className = 'slider__item';
-    arr_sliderDot[i].className = 'slider__dot';
-  });
-
-  if (counter > 0) {
-    counter--;
-  } else {
-    counter = arr_sliderItems.length - 1;
-  }
-
-  arr_sliderItems[counter].className += ' slider__item_active';
-  arr_sliderDot[counter].className += ' slider__dot_active';
+  let i = getImageNumber(arr_sliderItems, 'slider__item slider__item_active');
+  i = (i == 0) ? 4 : (i - 1);
+  drawing(arr_sliderItems, i, 'slider__item', ' slider__item_active');
+  drawing(arr_sliderDot, i, 'slider__dot', ' slider__dot_active');
 }
 
 sliderNext.onclick = function () {
-  arr_sliderItems.forEach((item, i) => {
-    item.className = 'slider__item';
-    arr_sliderDot[i].className = 'slider__dot';
-  });
-
-  if (counter < arr_sliderItems.length - 1) {
-    counter++;
-  } else {
-    counter = 0;
-  }
-
-  arr_sliderItems[counter].className += ' slider__item_active';
-  arr_sliderDot[counter].className += ' slider__dot_active';
+  let i = getImageNumber(arr_sliderItems, 'slider__item slider__item_active');
+  i = (i == 4) ? 0 : (i + 1);
+  drawing(arr_sliderItems, i, 'slider__item', ' slider__item_active');
+  drawing(arr_sliderDot, i, 'slider__dot', ' slider__dot_active');
 }
 
 arr_sliderDot.forEach((item, i) => {
   item.onclick = function () {
-    arr_sliderDot.forEach((item, i) => {
-      item.className = 'slider__dot';
-      arr_sliderItems[i].className = 'slider__item';
-    });
-    
-    item.className += ' slider__dot_active';
-    arr_sliderItems[i].className += ' slider__item_active';
-    counter = i;
+    drawing(arr_sliderItems, i, 'slider__item', ' slider__item_active');
+    drawing(arr_sliderDot, i, 'slider__dot', ' slider__dot_active');
   }
 });
